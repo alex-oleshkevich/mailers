a50ab2fa10d2841c896023e75077f6c7294a68cb
alex.oleshkevich <alex.oleshkevich@gmail.com>
2021-08-13 17:59:34 +0300

    Fix plugin example.

--- examples/plugins.py
+++ examples/plugins.py
@@ -1,20 +1,34 @@
-"""This example demonstrates how you can create your own transport and register it as a custom protocol."""
+"""This example demonstrates how you can create mailer plugins to perform before/after sending actions.
+
+You need to setup several environment variables before you can use this script:
+
+* MAILERS_RECIPIENT - a recipient's email ("To" header)
+* MAILERS_FROM_ADDRESS - a sender address ("From" header)
+* MAILER_URL - mailer configuration URL
+"""
 import asyncio
+import os
+from email.message import Message
 
 from mailers import BasePlugin, EmailMessage, create_mailer
 
 
 class PrintPlugin(BasePlugin):
-    async def on_before_send(self, message: EmailMessage) -> None:
+    async def on_before_send(self, message: Message) -> None:
         print('sending message')
 
-    async def on_after_send(self, message: EmailMessage) -> None:
+    async def on_after_send(self, message: Message) -> None:
         print('message has been sent')
 
 
 async def main():
-    message = EmailMessage(to='root@localhost', subject='Hello', text_body='World', from_address='reply@localhost')
-    mailer = create_mailer('null://', plugins=[PrintPlugin()])
+    message = EmailMessage(
+        to=os.environ.get('MAILERS_RECIPIENT', 'root@localhost'),
+        subject='Plugin test',
+        text_body='Hello, this is a test message.',
+        from_address=os.environ.get('MAILERS_FROM_ADDRESS', 'root@localhost'),
+    )
+    mailer = create_mailer(os.environ.get('MAILER_URL', 'null://'), plugins=[PrintPlugin()])
     await mailer.send(message)
 
 
