6a6b6c9b51376e68db7fdcc9fd9a955804c41bcb
alex.oleshkevich <alex.oleshkevich@gmail.com>
2021-08-13 18:01:54 +0300

    Update custom protocols example.

--- examples/custom_protocols.py
+++ examples/custom_protocols.py
@@ -1,7 +1,16 @@
-"""This example demonstrates how you can create your own transport and register it as a custom protocol."""
+"""This example demonstrates how you can create your own transport and register it as a custom protocol.
+
+You need to setup several environment variables before you can use this script:
+
+* MAILERS_RECIPIENT - a recipient's email ("To" header)
+* MAILERS_FROM_ADDRESS - a sender address ("From" header)
+* MAILER_URL - mailer configuration URL
+"""
 import asyncio
+import os
 import sys
 import typing as t
+from email.message import Message
 
 from mailers import BaseTransport, EmailMessage, EmailURL, Transport, add_protocol_handler, create_mailer
 
@@ -10,12 +19,11 @@ class PrintTransport(BaseTransport):
     def __init__(self, stream: t.IO) -> None:
         self.stream = stream
 
-    async def send(self, message: EmailMessage) -> None:
+    async def send(self, message: Message) -> None:
         self.stream.write(str(message))
 
     @classmethod
     def from_url(cls, url: t.Union[str, EmailURL]) -> t.Optional[Transport]:
-        print(url.options)
         stream_name = url.options.get('stream')
         if stream_name == 'stdout':
             return PrintTransport(sys.stdout)
@@ -29,7 +37,12 @@ add_protocol_handler('print', PrintTransport)
 
 
 async def main():
-    message = EmailMessage(to='root@localhost', subject='Hello', text='World', from_address='reply@localhost')
+    message = EmailMessage(
+        to=os.environ.get('MAILERS_RECIPIENT', 'root@localhost'),
+        subject='Attachments test',
+        text_body='Hello, this is a test message with attachments.',
+        from_address=os.environ.get('MAILERS_FROM_ADDRESS', 'root@localhost'),
+    )
     mailer = create_mailer('print://?stream=stdout')  # or stream=stderr
     await mailer.send(message)
 
