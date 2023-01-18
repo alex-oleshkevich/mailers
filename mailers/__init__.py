7353cd8224e8016188dbf1469d1a89e2b1989ac3
alex.oleshkevich <alex.oleshkevich@gmail.com>
2023-01-18 20:48:42 +0300

    remove pytest plugin from exports

--- mailers/__init__.py
+++ mailers/__init__.py
@@ -4,7 +4,6 @@ from mailers.factories import create_transport_from_url
 from mailers.mailer import Mailer, TemplatedMailer
 from mailers.message import Email
 from mailers.preprocessors import Preprocessor
-from mailers.pytest_plugin import Mailbox
 from mailers.signers import Signer
 from mailers.transports import (
     FileTransport,
@@ -32,5 +31,4 @@ __all__ = [
     "TemplatedMailer",
     "create_transport_from_url",
     "MultiTransport",
-    "Mailbox",
 ]
