b31dbce1d7cc0ce40f49c7d0bee23d910f944f41
alex.oleshkevich <alex.oleshkevich@gmail.com>
2024-11-30 18:33:04 +0100

    Fix linting issues

--- tests/preprocessors/test_html_comments_removal.py
+++ tests/preprocessors/test_html_comments_removal.py
@@ -32,6 +32,16 @@ def test_html_comment_remover_with_multiple_comment() -> None:
     assert message.get_content() == '<p class="text">hello</p>\n'
 
 
+def test_html_comment_remover_with_multiline_comment() -> None:
+    html = """<!-- I am a comment
+     This is the second line.--><p class="text">hello</p>\n"""
+    message = EmailMessage()
+    message.set_content(html, subtype="html", charset="utf-8")
+    message = remove_html_comments(message)
+
+    assert message.get_content() == '<p class="text">hello</p>\n'
+
+
 def test_css_inliner_with_text_only_message() -> None:
     html = """<!-- I am a comment -->but I am not html\n"""
     message = EmailMessage()
@@ -47,10 +57,7 @@ def test_css_inliner_with_multipart_message() -> None:
     message.add_alternative(html, subtype="html", charset="utf-8")
     message = remove_html_comments(message)
 
-    assert (
-        message.get_payload()[0].get_content()
-        == '<!-- I am a comment --><p class="text">hello</p>\n'
-    )
+    assert message.get_payload()[0].get_content() == '<!-- I am a comment --><p class="text">hello</p>\n'
     assert message.get_payload()[1].get_content() == '<p class="text">hello</p>\n'
 
 
@@ -62,13 +69,9 @@ def test_css_inliner_with_multipart_with_attachments_message() -> None:
 
     message = remove_html_comments(message)
     assert (
-        message.get_payload()[0].get_payload()[0].get_content()
-        == '<!-- I am a comment --><p class="text">hello</p>\n'
+        message.get_payload()[0].get_payload()[0].get_content() == '<!-- I am a comment --><p class="text">hello</p>\n'
     )
 
-    assert (
-        message.get_payload()[0].get_payload()[1].get_content()
-        == '<p class="text">hello</p>\n'
-    )
+    assert message.get_payload()[0].get_payload()[1].get_content() == '<p class="text">hello</p>\n'
 
     assert base64.b64decode(message.get_payload(1).get_payload()) == html.encode()
